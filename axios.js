3b124fccccb303b13566bf643b32cef2c78a288f
zhouchilly <zhouchilly@gmail.com>
2018-08-25 15:44:41 +0800

    Minor fix

--- axios.js
+++ axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import querystring from 'querystring';
+axios.defaults.baseURL = 'https://api.example.com';
+axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 const request = {
     handleData: function (res, resolve, reject) {
