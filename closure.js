d49c10567ed9100353037ae6511a1cc635a2cd9d
zhouchilly <zhouchilly@gmail.com>
2019-04-05 21:31:03 +0800

    feat(closure): update closure

--- closure.js
+++ closure.js
@@ -1,6 +1,21 @@
 // 闭包的用途
-// 1 函数作为返回值
+// 1 闭包通常用来创建内部变量，使得这些变量不能被外部随意修改，同时又可以通过指定的函数接口来操作
+function foo() {
+	var secret = 'secret';
+	return {
+		getSecret: function() {
+			return sercet;
+		},
+		setSecret: function(newSecret) {
+			secret = newSecret;
+		}
+	}
+}
+foo.getSecret(); // 'secret'
+foo.setSecret('newSecret');
+foo.getSecret(); // 'newSecret'
 
+// 2 函数作为返回值
 function outer() {
 	var local = 2;
 	function inner() {
@@ -12,8 +27,7 @@ var fn = outer();
 console.log(fn());  //2
 console.log(local); //local is not defined, 直接访问报错
 
-// 2 函数作为参数被传递
-
+// 3 函数作为参数被传递
 function outer() {
 	var local = 'zhou';
 	function inner() {
@@ -25,3 +39,5 @@ function fn(args) {
 	args();
 }
 outer(); //'zhou'
+
+
